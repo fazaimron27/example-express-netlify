956d166562a8a88798d931840719d703c3c4c1aa
Faza Iman Imron <cincaumyself@gmail.com>
2023-08-27 17:31:16 +0700

    resolve structure

--- app/routes/index.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-module.exports = (app) => {
-  router.get("/", (req, res) => {
-    res.render("index");
-  });
-
-  app.use(`/`, router);
-};
--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -7,7 +7,7 @@ app.set("views", path.join(__dirname, "app/views"));
 app.set("view engine", "ejs");
 app.engine("ejs", require("ejs").__express);
 
-require("../../app/routes")(app);
+require("./app/routes")(app);
 
 const handler = serverless(app);
 module.exports = { handler };
