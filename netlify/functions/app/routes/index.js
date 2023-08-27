e364b0f8d98d9a6884178c34b2af2b693aa94fd9
Faza Iman Imron <cincaumyself@gmail.com>
2023-08-27 17:36:14 +0700

    fix error ejs

--- netlify/functions/app/routes/index.js
+++ netlify/functions/app/routes/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 module.exports = (app) => {
   router.get("/", (req, res) => {
-    res.render("index");
+    res.render("index.ejs");
   });
 
   app.use(`/`, router);
