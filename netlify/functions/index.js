5c9ae80ef7a6a0489ded46cab5c635210629f63e
Faza Iman Imron <cincaumyself@gmail.com>
2023-08-27 17:20:15 +0700

    fix error ejs

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -5,6 +5,7 @@ const serverless = require("serverless-http");
 const app = express();
 app.set("views", path.join(__dirname, "../../app/views"));
 app.set("view engine", "ejs");
+app.engine("ejs", require("ejs").__express);
 
 require("../../app/routes")(app);
 
