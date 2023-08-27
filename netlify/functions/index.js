cbc80ac187eb537df61b59e3cdd9af0656160350
Faza Iman Imron <cincaumyself@gmail.com>
2023-08-27 17:13:11 +0700

    add ejs template

---
+++ netlify/functions/index.js
@@ -0,0 +1,12 @@
+const path = require("path");
+const express = require("express");
+const serverless = require("serverless-http");
+
+const app = express();
+app.set("views", path.join(__dirname, "../../app/views"));
+app.set("view engine", "ejs");
+
+require("../../app/routes")(app);
+
+const handler = serverless(app);
+module.exports = { handler };
